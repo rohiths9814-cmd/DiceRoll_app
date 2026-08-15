13873843eac8f8144536c48e9fa1dd774dc65fda
rohiths9814-cmd <rohiths9814@gmail.com>
2026-08-15 17:29:42 +0530

    Learned to create the files

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -16,13 +17,7 @@ class GradientContainer extends StatelessWidget {
             ),
           ),
           child:  Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
+            child: StyledText(),
           ),
         );
   }
