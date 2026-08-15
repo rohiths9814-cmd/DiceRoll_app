bb8e5a9ea013541515d05686511e465d37647c18
rohiths9814-cmd <rohiths9814@gmail.com>
2026-08-15 17:38:41 +0530

    Learned to create the files

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:my_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colour1, this.colour2, {super.key});
+
+  final Color colour1;
+  final Color colour2;
+
   @override
   Widget build(context) {
     return Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 31, 0, 84),
-                const Color.fromARGB(255, 18, 178, 84)
-              ],
+              colors: [ colour1, colour2],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
